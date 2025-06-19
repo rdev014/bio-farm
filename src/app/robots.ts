07ffaccb64d1300c7b3357c512992ce4b9ba3797
rdev014 <rdev01431@gmail.com>
2025-06-19 23:46:27 +0530

    fix(robots): update sitemap URL to point to the correct location

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://arkinbio.netlify.app/sitemap.xml',
+    sitemap: 'https://arkin-phi.vercel.app/sitemap.xml',
   }
 }
