9bc413424f92f9f6a9ebcad9883bbf682be50faf
rdev014 <rdev01431@gmail.com>
2025-06-30 00:20:13 +0530

    feat: implement metadata generation for blog posts to improve SEO

--- src/app/(client)/blogs/[slug]/page.tsx
+++ src/app/(client)/blogs/[slug]/page.tsx
@@ -1,7 +1,36 @@
 import { showBlog } from '@/actions/blog'
 import BlogPost from '@/components/blogs/BlogPost'
 import { notFound } from 'next/navigation';
+import type { Metadata } from 'next'
+ 
+type Props = {
+  params: Promise<{ slug: string }>
+}
+ 
+export async function generateMetadata(
+  { params}: Props,
+): Promise<Metadata> {
+  // read route params
+  const { slug } = await params
+ 
+// fetch data
+const blog = await showBlog({slug})
 
+
+if (!blog) {
+  return {
+    title: 'Blog Not Found',
+    description: 'The blog post you are looking for does not exist.',
+   
+  }
+}
+
+return {
+  title: blog.seo?.metaTitle ?? 'Blog Post: ' + blog.title,
+  description: blog.seo?.metaDescription ?? 'A blog post about ' + blog.title,
+  
+}
+}
 export default async function Page({
   params,
 }: {
