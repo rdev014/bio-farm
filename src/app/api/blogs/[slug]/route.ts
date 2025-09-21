57e34e0351e3da7b08bb7b9e477927eef545670f
rdev014 <rdev01431@gmail.com>
2025-09-22 01:16:52 +0530

    api finish

---
+++ src/app/api/blogs/[slug]/route.ts
@@ -0,0 +1,19 @@
+import connectDb from "@/lib/db";
+import { Blog } from "@/models/blogSchema";
+import { NextResponse } from "next/server";
+interface Params{slug:string}
+
+export async function GET(request: Request, {params}:{params:Params}) {
+    try {
+        const {slug} = params;
+        await connectDb();
+        const blog  = await Blog.find({slug})
+        if (!blog){
+            return NextResponse.json({error:"blog not found"},{status:404})
+        }
+        return NextResponse.json(blog,{status:200})
+    
+    } catch (error) {
+    return NextResponse.json({error:"something went wrong try again later"},{status:500})
+    }
+}
